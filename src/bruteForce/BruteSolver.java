e311ef2d1b368e89ec8e09ecfdfa72957a6db8a7
yarivs <PARSUS@PARSUS-PC>
2015-05-15 12:36:54 +0300

    brute documentaion

--- src/bruteForce/BruteSolver.java
+++ src/bruteForce/BruteSolver.java
@@ -7,12 +7,21 @@ import java.util.stream.IntStream;
 
 import game.Board;
 
+/**
+ * Solves Sudoku board using brute-force.
+ */
 public class BruteSolver {
 
+	/**
+	 * Solves sudoku board. Sets the solution in-place.
+	 * 
+	 * @param b
+	 *            the board
+	 * @return true if and only if a solution was found.
+	 */
 	public static boolean solve(Board b) {
 		if (b.isFull()) {
 			return true;
-			// return Validator.validate(b);
 		}
 
 		for (int i = 0; i < Board.BOARD_WIDTH; i++) {
@@ -40,12 +49,24 @@ public class BruteSolver {
 		return true;
 	}
 
+	/**
+	 * Returns a list of feasible assignments according to the state of the
+	 * board.
+	 * 
+	 * @param b
+	 *            the state of the board
+	 * @param i
+	 *            the row's index of the cell
+	 * @param j
+	 *            the col's index of the set
+	 * @return feasible assignments to cell[i][j]
+	 */
 	public static List<Byte> getPossibilities(Board b, int i, int j) {
 		List<Byte> row = b.getRows().get(i);
 		List<Byte> col = b.getCols().get(j);
 		i /= Board.BLOCK_WIDTH;
 		j /= Board.BLOCK_WIDTH;
-		List<Byte> block = b.getBlocks().get(3 * i + j);
+		List<Byte> block = b.getBlocks().get(Board.BLOCK_WIDTH * i + j);
 
 		List<Integer> possibilities = IntStream.range(1, 10).boxed()
 				.filter(x -> !row.contains(x.byteValue()))
@@ -53,6 +74,7 @@ public class BruteSolver {
 				.filter(x -> !block.contains(x.byteValue()))
 				.collect(Collectors.toList());
 
+		// Converting to List<Byte>
 		List<Byte> bPossibilities = new ArrayList<>();
 		for (Integer num : possibilities) {
 			bPossibilities.add(num.byteValue());
@@ -61,7 +83,4 @@ public class BruteSolver {
 		return bPossibilities;
 	}
 
-	public static void solve(Board b, int i, int j, int value) {
-
-	}
 }
